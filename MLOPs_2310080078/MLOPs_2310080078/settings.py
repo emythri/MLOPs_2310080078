75fffddf7c1aaff2b010ddc86e5cd9ab7a501c25
emythri <eelapantimythri@gmail.com>
2024-11-17 12:02:56 +0530

    final commit

--- MLOPs_2310080078/MLOPs_2310080078/settings.py
+++ MLOPs_2310080078/MLOPs_2310080078/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-33)sp#ydy-e1^plxfyoa=9ldw80yz2pns6gzc^_#ydb(#f(uvh
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
